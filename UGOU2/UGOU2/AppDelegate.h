ffffaf6bbc7bd97451d645d5c6e240243ceae9b1
songhailong <997496837@qq.com>
2018-08-21 16:47:09 +0800

    第一次

---
+++ UGOU2/UGOU2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UGOU2
+//
+//  Created by 靓萌服饰靓萌服饰 on 2018/8/21.
+//  Copyright © 2018年 靓萌服饰靓萌服饰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
